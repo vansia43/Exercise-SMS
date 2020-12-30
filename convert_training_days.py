23452fdfc72e2b78b7884bffe419b16ce098747c
Danielle Vansia <41837012+vansia43@users.noreply.github.com>
2020-12-30 16:05:16 -0500

    Add files via upload

---
+++ convert_training_days.py
@@ -0,0 +1,49 @@
+import csv
+import json
+from datetime import datetime
+
+# Define the header names so we can specify them when reading the CSV
+fieldnames = (
+    "Date","DayoftheWeek","Activity","DaysTrained"
+
+)
+
+# Open both files so we can interact with them
+# Using the `with` keyword lets us close the files automatically after these
+# with blocks end and we're done writing and reading the files
+
+with open('./days.csv', 'r') as csvfile:
+    with open('./days.json', 'w') as jsonfile:
+            # `next` will simply skip over the header row in the csvfile
+        next(csvfile)
+            # We use the csv library to create a 'reader' of the file
+            # This reader patse through the csvfile and the headers
+            # and allow us to interact with it as a Python object
+        reader = csv.DictReader(csvfile, fieldnames)
+            # This creates an empty dictionary to hold the final set of
+            # data that we'll eventually dump into the JSON file
+        final_data = {}
+            # Now we use the reader to iterate over all the rows of the CSV
+            # (except for the header) and then keep the values we want
+        final = []
+        for row in reader:
+                # We also restructure the data so that it exists as
+                # a set of date keys with the value as a dictionary of
+                # different data elements from the CSV.
+                # "Date","Open","High","Low","Close","Volume","Adj Close"
+            final_data = {
+                    "Date": row["Date"],
+                    "DayWeek": row["DayoftheWeek"],
+                    "Activity": row["Activity"],
+                    "DaysTrained": row["DaysTrained"]
+                }
+            final.append(final_data)
+        sorted_date = sorted(final, key=lambda x: datetime.strptime(x['Date'], '%m-%d-%Y'))
+
+        fin = sorted_date
+            # Finally, we use the json library to output the final_data
+            # dictionary to the jsonfile we opened earlier
+        json.dump(fin, jsonfile)
+            # And then we write a final newline to the end of the file
+            # (this is just a best practice)
+        jsonfile.write('\n')
---
+++ main.py
@@ -0,0 +1,71 @@
+
+import schedule
+import time
+import json
+from datetime import datetime
+from twilio.rest import Client
+
+
+def job():
+
+
+
+    ## set twillo api key
+    account_sid = ''
+    auth_token = ''
+    client = Client(account_sid, auth_token)
+
+    ## get today's date. and use that to look up which exersice to do.
+    curday = datetime.now().strftime("%m-%d-%Y")
+
+    ## get data from days json file and pull up the activity for todays date.
+    with open('./days.json', 'r') as days:
+        day = json.loads(days.read())
+        for keyval in day:
+            if curday == keyval['Date']:
+                activity =  keyval['Activity']
+                daysTrained =  keyval['DaysTrained']
+                dayWeek =  keyval['DayWeek']
+                dayTrainNum = int(daysTrained)
+
+    exerciseBlock = ("Hi Dani!! Here is your exercise plan for the day to keep you motivated!! \nToday is " + dayWeek +" "+ curday + ". You will be doing the following activity--" + activity + ". You have trained  " + daysTrained + " days so far!! \n\n" )
+
+    ## get the inspirational quote based on how many days trained so far.
+    with open('./quotes.json', 'r') as quotes:
+        file = json.loads(quotes.read())
+        quote = file[dayTrainNum]['text']
+        author = file[dayTrainNum]['author']
+    quoteBlock = ("Inspirational quote of the day\n" + quote+ " --said by " + author )
+
+
+    # get the youtube link of an exersice that needs a video to do
+    linkBlock = " "
+    if activity == "barre":
+        linkBlock = ("Here is a link to some youtube videos for "+activity+ " activity " +"https://www.youtube.com/results?search_query=barre+workout+20+minutes+" + " Enjoy your workout \n\n")
+    if activity == "yoga":
+        linkBlock = ("Here is a link to some youtube videos for "+activity+ " activity " +"https://www.youtube.com/results?search_query=yoga+workout+30+minutes+" + " Enjoy your workout \n\n")
+    if activity == "HIIT":
+        linkBlock = ("Here is a link to some youtube videos for "+activity+ " activity " +"https://www.youtube.com/results?search_query=hiit+workout+15+minutes+" + " Enjoy your workout \n\n")
+
+    # combine all of the messages to be ready to send with twilio
+    body = (exerciseBlock+linkBlock+quoteBlock)
+    print(body)
+
+    # contruct a message to send using twilio use the text as the body with the combinded messages
+    message = client.messages.create(
+                              from_='+',
+                              body = body,
+                              to ='+'
+                          )
+    print(message.sid)
+
+
+
+## scheudle a job to run at a certain interval this will then execute the job function
+# schedule.every(10).seconds.do(job)
+schedule.every().day.at("08:07").do(job)
+
+# create an infitine loop that will always run the service
+while True:
+    schedule.run_pending()
+    time.sleep(1)
